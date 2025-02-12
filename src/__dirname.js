b176ecffa621a8e44b831b94b921921bbd04e835
Sebastian0021 <sebastianalefuentespe@gmail.com>
2025-02-12 15:49:23 -0300

    agregando multer a la entrega

--- src/__dirname.js
+++ src/__dirname.js
@@ -4,10 +4,4 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const getDirname = (meta) => {
-  const __filename = fileURLToPath(meta);
-  const __dirname = dirname(__filename);
-  return __dirname;
-};
-
 export default __dirname;
